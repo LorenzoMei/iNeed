41866cf7bc2ed385484fba8f215f9b0eeca9e896
LorenzoMei <45484506+LorenzoMei@users.noreply.github.com>
2020-02-03 17:49:10 +0100

    #19, #20

--- src/logic/entity/User.java
+++ src/logic/entity/User.java
@@ -5,8 +5,24 @@ public class User {
 	private String username;
 	private String passw;
 	
-	public User(String username, String passw) {
+	public User() {
+		this.username = "";
+		this.passw = "";
+	}
+	
+	public void setUsername(String username) {
 		this.username = username;
+	}
+	
+	public void setPassw(String passw) {
 		this.passw = passw;
+	}
+	
+	public String getUsername() {
+		return this.username;
+	}
+	
+	public String getPassw() {
+		return this.passw;
 	}
 }
--- src/logic/publishanad/Post.java
+++ src/logic/publishanad/Post.java
@@ -5,9 +5,33 @@ public class Post {
 	private String body;
 	private String type;
 	
-	public Post(String title, String body, String type) {
+	public Post() {
+		this.title = "";
+		this.body = "";
+		this.type = "";
+	}
+	
+	public void setTitle(String title) {
 		this.title = title;
+	}
+	
+	public void setBody(String body) {
 		this.body = body;
+	}
+	
+	public void setType(String type) {
 		this.type = type;
+	}
+	
+	public String getTitle() {
+		return this.title;
+	}
+	
+	public String getBody() {
+		return this.body;
+	}
+	
+	public String getType() {
+		return this.type;
 	}
 }
