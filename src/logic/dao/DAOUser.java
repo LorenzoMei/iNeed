2171ede88cbaf76f50fc3c564df72920dac2764f
Daniele La Prova <32265040+Torkin1@users.noreply.github.com>
2020-02-05 03:26:37 +0100

    #56

--- src/logic/dao/DAOUser.java
+++ src/logic/dao/DAOUser.java
@@ -3,6 +3,6 @@ package logic.dao;
 import logic.entity.User;
 
 public interface DAOUser {
-	public User loadUser(User user, String username) throws UserNotFoundException;
+	public void loadUser(User user, String username) throws UserNotFoundException;
 	public void storeUser(User user);
 }
--- src/logic/dao/DAOUserJson.java
+++ src/logic/dao/DAOUserJson.java
@@ -5,6 +5,8 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level; 
 import java.util.logging.Logger; 
 import org.json.simple.*;
@@ -12,10 +14,10 @@ import org.json.simple.parser.*;
 
 import logic.entity.User;
 
-public class DAOUserJson implements DAOUser{
+public class DAOUserJson extends DAOJson implements DAOUser{
 	
 	Logger logger = Logger.getLogger(DAOUserJson.class.getName());
-	private String pathnameFile = "db/json/users/"; 
+	private String pathnameFile = "db/json/User/"; 
 
 	private static DAOUserJson ref = null;
 	
@@ -28,70 +30,36 @@ public class DAOUserJson implements DAOUser{
 		return ref;
 	}
 	
-	private Method getGetterOrSetter(String getOrSet, String attrName, Object obj) throws NoSuchMethodException {
-		
-		Method[] methods = obj.getClass().getMethods();
-		for (int j = 0; j < methods.length; j ++) {
-			if (methods[j].getName().contains(getOrSet) && methods[j].getName().contains(attrName.substring(0, 1).toUpperCase() + attrName.substring(1))) {
-				return methods[j];
-			}
-		}
-		throw new NoSuchMethodException();
-	}
-	
-	public User loadUser(User user, String username) throws UserNotFoundException{
+	public void loadUser(User user, String username) throws UserNotFoundException{
 		
 //		Searches in every node of the root in DB for a User with the same username and passw as the ones provided.
 //		@ return User if retrieved, null otherwise
 		
-		String fileName = pathnameFile + username + ".json";
-		File userDB = new File(fileName);
-		try (FileReader reader = new FileReader(userDB)){
-			JSONArray root = (JSONArray) JSONValue.parseWithException(reader);
-			for (int i = 0; i < root.size(); i ++) {
-				JSONObject currentNode = (JSONObject) root.get(i);
-				String currentUsername = (String) currentNode.get("username");
-				if (currentUsername.equals(username)) {
-					Field[] attributes = user.getClass().getDeclaredFields();
-					for (int j = 0; j < attributes.length; j ++) {
-						if (Modifier.isPrivate(attributes[j].getModifiers())) {
-							this.getGetterOrSetter("set", attributes[j].getName(), user).invoke(user, currentNode.get(attributes[j].getName()));
-						}
-					}
-				}
-			}	
-		} catch (FileNotFoundException e) {
-			logger.log(Level.SEVERE, "file" + fileName + " not found");
+		
+			List <String> primaryKeyNames = new ArrayList <String>();
+			primaryKeyNames.add("username");
+			List <String> primaryKeyValues = new ArrayList <String>();
+			primaryKeyValues.add(username);
+		try {
+			this.load(user, primaryKeyNames, primaryKeyValues);
+		} catch (ElementInDBNotFoundException e) {
+			logger.log(Level.SEVERE, "file" + e.getPath() + " not found");
 			throw new UserNotFoundException();
 		} 
 		
 		catch (IOException | ParseException | NoSuchMethodException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
 			logger.log(Level.SEVERE, e.toString());
-		}
-		
-		return user;		
+		}	
 	}
 	
 	public void storeUser(User user) {
 		
 //		It stores user data in a JSON array, coping values of all User attributes using reflection
-//		TODO: 	for each user exist a dictionary node, and every node should be added to the array
-//				root (or maybe we can write every user in his own file?), so an append write mode
-//				should be implemented.
 		
-		File userDB = new File(pathnameFile + user.getUsername() + ".json");
-		try (FileWriter writer = new FileWriter(userDB)) {
-			JSONObject values = new JSONObject();
-			JSONArray root = new JSONArray();
-			Field[] attributes = user.getClass().getDeclaredFields();
-						
-			for (int i = 0; i < attributes.length; i ++) {
-				if (Modifier.isPrivate(attributes[i].getModifiers())) {
-					values.put(attributes[i].getName(), this.getGetterOrSetter("get", attributes[i].getName(), user).invoke(user, (Object[]) null));
-				}
-			}
-			root.add(values);
-			root.writeJSONString(writer);
+		List<String> primaryKeyNames = new ArrayList<String>();
+		primaryKeyNames.add("user");
+		try {
+			this.store(user, primaryKeyNames);
 			
 		} catch (IOException | IllegalAccessException | IllegalArgumentException | SecurityException | InvocationTargetException | NoSuchMethodException e) {
 			logger.log(Level.SEVERE, e.toString());
