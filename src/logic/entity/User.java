ec9c5f2951838361edf045f6f0532dd95a910217
mihaiJianu <45464636+mihaiJianu@users.noreply.github.com>
2020-02-09 10:12:42 +0100

    #78

--- src/logic/entity/User.java
+++ src/logic/entity/User.java
@@ -1,21 +1,29 @@
 package logic.entity;
 
+import java.time.LocalDate;
+
 public class User {
 	
 	private String username;
 	private String passw;
 	private String city;
 	private String email;
+	private String name;
+	private String surName;
+	private LocalDate bDate;
 
-	
 	
 	public User() {
 		this.username = "";
 		this.passw = "";
 		this.city = "";
 		this.email = "";
+		this.name = "";
+		this.surName = "";
+		this.bDate = null;
 	}
 	
+	//all the sets on attributes
 	public void setUsername(String username) {
 		this.username = username;
 	}
@@ -27,11 +35,25 @@ public class User {
 	public void setCity(String city) {
 		this.city = city;
 	}
+	
 	public void setEmail(String email) {
 		this.email = email;
 	}
 	
+	public void setName(String name) {
+		this.name = name;
+	}
 	
+	public void setSurName(String surName) {
+		this.surName = surName;
+	}
+	
+	public void setBirthDate(LocalDate bDate) {
+		this.bDate = bDate;
+	}
+	
+	
+	//all the gets on attributes
 	public String getEmail() {
 		return this.email;
 	}
@@ -47,4 +69,16 @@ public class User {
 	public String getPassw() {
 		return this.passw;
 	}
+	public String getSurName() {
+		return this.surName;
+	}
+	
+	public String getname() {
+		return this.name;
+	}
+	
+	public LocalDate getBirthDate() {
+		return this.bDate;
+	}
+	
 }
