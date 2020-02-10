dd3d8b967f4319f66742e987749878a790cfc984
mihaiJianu <45464636+mihaiJianu@users.noreply.github.com>
2020-02-10 19:46:14 +0100

    #92

---
+++ src/logic/signup/SignUpBean.java
@@ -0,0 +1,93 @@
+package logic.signup;
+
+import java.util.Calendar;
+
+public class SignUpBean {
+	
+	private static SignUpBean instance;
+	
+	private String username;
+	private String passw;
+	private String email;
+	private String city;
+	private String name;
+	private String surName;
+	private Calendar bDate;
+	
+	public static SignUpBean getInstance() {
+		if(instance == null)
+			instance = new SignUpBean();
+		return instance;
+	}
+	
+	public SignUpBean() {
+		this.username = "";
+		this.passw = "";
+		this.email = "";
+		this.city = "";
+		this.name = "";
+		this.surName = "";
+		this.bDate = null;
+	}
+	
+	//all the sets on attributes
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public void setPassword(String passw) {
+		this.passw = passw;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setSurName(String surName) {
+		this.surName = surName;
+	}
+	
+	public void setBirthDate(Calendar userBirthDate) {
+		this.bDate = userBirthDate;
+	}
+	
+	//all the gets on attributes
+
+	public String getUsername() {
+		return this.username;
+	}
+	
+	public String getPassword() {
+		return this.passw;
+	}
+	
+	public String getEmail() {
+		return this.email;
+	}
+	
+	public String getCity() {
+		return this.city;
+	}
+	
+	public String getSurName() {
+		return this.surName;
+	}
+	
+	public String getname() {
+		return this.name;
+	}
+	
+	public Calendar getBirthDate() {
+		return this.bDate;
+	}
+	
+}
--- src/logic/signup/SignUpControllerInterface.java
+++ src/logic/signup/SignUpControllerInterface.java
@@ -1,7 +1,5 @@
 package logic.signup;
 
-import logic.beans.SignUpBean;
-
 public interface SignUpControllerInterface {
 
 		public void signUp(SignUpBean bean) throws UsernameAlreadyTakenException;
