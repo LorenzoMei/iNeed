f430e1030ccf18e352cfe14764fff4a0bd698709
LorenzoMei <45484506+LorenzoMei@users.noreply.github.com>
2020-02-11 15:07:42 +0100

    #101

--- src/logic/signup/SignUpController.java
+++ src/logic/signup/SignUpController.java
@@ -32,10 +32,10 @@ public class SignUpController implements SignUpControllerInterface{
 		
 		User userTemp = new User();
 		
-		DAOUser DAO = (DAOUser) DAOFactory.getReference().getDAOReference("User");
+		DAOUser dao = (DAOUser) DAOFactory.getReference().getDAOReference("User");
 		
 		try {
-			DAO.loadUser(userTemp, username);
+			dao.loadUser(userTemp, username);
 		} catch (UserNotFoundException e) {
 			e.printStackTrace();
 			userTemp.setUsername(username);
@@ -45,7 +45,7 @@ public class SignUpController implements SignUpControllerInterface{
 			userTemp.setName(name);
 			userTemp.setSurName(surName);
 			userTemp.setBDate(bDate);
-			DAO.storeUser(userTemp);
+			dao.storeUser(userTemp);
 			
 			return;
 		}
