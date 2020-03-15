e1a22adaa22e49d25332838a3a9c2adf1413bc49
Daniele La Prova <32265040+Torkin1@users.noreply.github.com>
2020-03-15 17:49:47 +0100

    #186

--- src/logic/beans/KeyReviewsBean.java
+++ src/logic/beans/KeyReviewsBean.java
@@ -4,8 +4,7 @@ public enum KeyReviewsBean {
 	REVIEWSBEAN_USERNAMEWRITER("usernameWriter"),
 	REVIEWSBEAN_USERNAMETOREVIEW("usernameToReview"),
 	REVIEWSBEAN_VOTE("vote"),
-	REVIEWSBEAN_TEXT("text"),
-	REVIEWSBEAN_REVIEWSLIST("reviewsList");
+	REVIEWSBEAN_TEXT("text");
 	
 	private final String keyName;
 	
--- src/logic/dao/DAOReviews.java
+++ src/logic/dao/DAOReviews.java
@@ -1,11 +1,8 @@
 package logic.dao;
 
-import java.util.List;
-
 import logic.entity.Review;
 
 public interface DAOReviews {
 	
 	public void storeReview(Review review);
-	public void loadReview(List <Review> reviewsList, String usernameToReview);
 }
