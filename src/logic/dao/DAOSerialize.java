eb1e24851aff3f992b8cdfa1a2353824e88a3f73
Daniele La Prova <32265040+Torkin1@users.noreply.github.com>
2020-03-22 20:10:11 +0100

    build 263 failing

--- src/logic/dao/DAOSerialize.java
+++ src/logic/dao/DAOSerialize.java
@@ -1,74 +1,169 @@
 package logic.dao;
 
-
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public abstract class DAOSerialize {
+import logic.entity.Ad;
+import logic.misc.ReflectionMiscellaneous;
 
-	Logger logger = Logger.getLogger(this.getClass().getName());
+public class DAOAdSerialize extends DAOSerialize implements DAOAd{
 	
-	protected static final String PRIMARY_KEY_VALUES_SEPARATOR = "#";
-	protected static final String SERIALIZED_EXTENSION = ".ser";
-	protected static final String DATE_TIME_FORMAT_STANDARD = "YYYY-MM-dd-HH-mm-ss";
-	protected static final String DATE_TIME_SEPARATOR = "-";
+	public static final String LASTID_TYPE_OFFER = "OfferAd";
+	public static final String LASTID_TYPE_REQUEST  = "RequestAd";
+	public static final int LASTID_ZEROID = -1;
 	
-	protected String[] tokenize(File stored){
-		return this.tokenize(stored.getName());
-	}
+	Logger logger = Logger.getLogger(DAOAdSerialize.class.getName());
+	private static DAOAdSerialize ref = null;
 	
-	protected String[] tokenize(String storedName){
-		return storedName.split(DAOSerialize.PRIMARY_KEY_VALUES_SEPARATOR);
-	}
-	
-	protected String readDBPath() {
-//		TODO: stub
-		return "db" + File.separator + "serialized" + File.separator;
-	}
-
-	private String getPath(Object obj, List <String> primaryKeyValues){
-		StringBuilder stringBuilder = new StringBuilder();
-		stringBuilder.append(this.readDBPath() + obj.getClass().getSimpleName() + File.separator);
-		File test = new File(stringBuilder.toString());
-		if (!test.exists()) {
-			test.mkdirs();
-		}
-		for (int k = 0; k < primaryKeyValues.size(); k ++) {
-			stringBuilder.append(primaryKeyValues.get(k));
-			stringBuilder.append(DAOSerialize.PRIMARY_KEY_VALUES_SEPARATOR);
-		}
-		stringBuilder.append(DAOSerialize.SERIALIZED_EXTENSION);
-		return stringBuilder.toString();
-	}
-	
-	protected void store(Object obj, List <String> primaryKeyValues) throws IllegalAccessException, InvocationTargetException, NoSuchMethodException, IOException {
-		logger.log(Level.INFO, this.getPath(obj, primaryKeyValues));
-		try (
-				FileOutputStream dest = new FileOutputStream(this.getPath(obj, primaryKeyValues));
-				DAOSerializeOOS writer = new DAOSerializeOOS(dest);){
-			logger.log(Level.INFO, "Destination: {0}", dest);
-			writer.writeObject(obj);
+	private DAOAdSerialize() {
+//		p
+		logger.log(Level.INFO, "initializing {0}", this.getClass().getSimpleName());
+		
+		List<Field> supportedAds = ReflectionMiscellaneous.getSupported(this, "LASTID_TYPE");
+		
+		for (int i = 0; i < supportedAds.size(); i ++) {
+			try {
+				String pathLastId = this.readDBPath() + "AdId" + File.separator + supportedAds.get(i).get(null).toString();
+//				p
+				String toLog = String.format("checking if %s directory exists", pathLastId);
+				logger.log(Level.INFO, toLog);
+				File lastIdFile = new File(pathLastId);
+				if (!lastIdFile.exists()) {
+//					p
+					toLog = String.format("%s does not exists, creating it", pathLastId);
+					logger.log(Level.INFO, toLog);
+					AdId zeroId = new AdId();
+					zeroId.setLastId(LASTID_ZEROID);
+					zeroId.setType(supportedAds.get(i).get(null).toString());
+					this.storeLastId(zeroId);
+				}
+			} catch (IllegalArgumentException | IllegalAccessException e) {
+				String message = String.format("%s unable to initialize DAOAd ", e);
+				logger.log(Level.SEVERE, message);
+			}
 		}
 	}
-	protected void load(Object obj, List <String> primaryKeyValues) throws IOException, ClassNotFoundException, ElementInDBNotFoundException, IllegalAccessException, InvocationTargetException, NoSuchMethodException, InstantiationException {
+	
+	public static DAOAdSerialize getReference() {
+		
+		if (ref == null) {
+			ref = new DAOAdSerialize();
+		}
+		return ref;
+	}
+	
+	@Override
+	public void loadAd(Ad ad, int id) throws AdNotFoundException {
 		
 //		Searches in every node of the root in DB for a User with the same username and passw as the ones provided.
 //		@ return User if retrieved, null otherwise
 		
-		try (
-				FileInputStream src = new FileInputStream(this.getPath(obj, primaryKeyValues));
-				DAOSerializeOIS reader = new DAOSerializeOIS(src, obj);){
-			reader.readObject();
+		List <String> primaryKeyValues = new ArrayList <>();
+		primaryKeyValues.add(Integer.toString(id));
+		
+		try {
+			this.load(ad, primaryKeyValues);
+		} catch (ElementInDBNotFoundException e) {
+			throw new AdNotFoundException(e);
+		} catch (IOException | ClassNotFoundException | IllegalAccessException | InvocationTargetException | NoSuchMethodException | InstantiationException e) {
+			logger.log(Level.SEVERE, e.toString());
+		} 
+	}
+	
+	@Override
+	public int storeNewAd(Ad ad) {
+		AdId id = new AdId();
+		this.updateLastId(ad.getClass().getSimpleName());
+		this.loadLastId(id, ad.getClass().getSimpleName());
+		ad.setId(id.getLastId());
+//		p
+		logger.log(Level.INFO, "ad " + ad + " id set to " + ad.getId());
+		this.storeAd(ad);
+		return ad.getId();
+	}
+	
+	@Override
+	public void storeAd(Ad ad) {
+		List<String> primaryKeyValues = new ArrayList<>();
+		primaryKeyValues.add(String.format("%d", ad.getId()));
+		try {
+			this.store(ad, primaryKeyValues);
+		} catch (IOException | IllegalAccessException | IllegalArgumentException | SecurityException | InvocationTargetException | NoSuchMethodException e) {
+			logger.log(Level.SEVERE, e.toString());
+		}		 
+	}
+	
+	private void storeLastId(AdId id) {
+		List<String> primaryKeyNames = new ArrayList<>();
+		primaryKeyNames.add(id.getType());
+		try {
+			this.store(id, primaryKeyNames);
+		} catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException | IOException e) {
+			logger.log(Level.SEVERE, e.toString());
 		}
-		catch (FileNotFoundException e) {
-			throw new ElementInDBNotFoundException(this.getPath(obj, primaryKeyValues), e);
-		}
+	}
+	
+	private int updateLastId(String type) {
+		
+		int oldVal;
+		int newVal;
+		AdId lastId = new AdId();
+		
+		this.loadLastId(lastId, type);
+//		p
+		logger.log(Level.INFO, "old lastId is " + lastId.getLastId());
+		oldVal = lastId.getLastId();
+		newVal = ++ oldVal ;
+		lastId.setLastId(newVal);
+//		p
+		logger.log(Level.INFO, "storing new val of lastId: " + lastId.getLastId());
+		this.storeLastId(lastId);
+		
+		return newVal;
+	}
+	
+	private void loadLastRequestId(AdId id) {
+		this.loadLastId(id, DAOAdSerialize.LASTID_TYPE_REQUEST);
+	}
+	
+	private void loadLastOfferId(AdId id) {
+		this.loadLastId(id, DAOAdSerialize.LASTID_TYPE_OFFER);
+	}
+	
+	private void loadLastId(AdId id, String type) {
+		
+		List <String> primaryKeyValues = new ArrayList <>();
+		primaryKeyValues.add(type);
+		
+		try {
+			this.load(id, primaryKeyValues);
+		} catch (ElementInDBNotFoundException e) {
+			logger.log(Level.INFO, "file" + e.getPath() + " not found");
+			logger.log(Level.INFO, "create file " + e.getPath());
+		} catch (IOException | IllegalArgumentException | ClassNotFoundException | IllegalAccessException | InvocationTargetException | NoSuchMethodException | InstantiationException e) {
+			logger.log(Level.SEVERE, e.toString());
+		} 
+	}
+
+	@Override
+	public int getLastRequestId() {
+		AdId lastRequestId = new AdId();
+		this.loadLastRequestId(lastRequestId);
+		
+		return lastRequestId.getLastId();
+	}
+
+	@Override
+	public int getLastOfferId() {
+		AdId lastOfferId = new AdId();
+		this.loadLastOfferId(lastOfferId);
+		
+		return lastOfferId.getLastId();
 	}
 }
