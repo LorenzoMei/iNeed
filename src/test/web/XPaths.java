83ee1e6b8cbf7c559b601b9d785ff77364d0206d
LorenzoMei <45484506+LorenzoMei@users.noreply.github.com>
2020-05-21 23:44:48 +0200

    #5

--- src/test/web/XPaths.java
+++ src/test/web/XPaths.java
@@ -3,11 +3,19 @@ package test.web;
 public enum XPaths {
 	CHECKANSWERS_CONTACTINFO("//*[@id=\"contactInfo\"]"),
 	LOGIN_BLOGIN("//*[@id=\"login\"]"),
-	LOGIN_FPASSWORD("//*[@id=\"password\"]"),
 	LOGIN_FUSERNAME("//*[@id=\"username\"]"),
+	LOGIN_FPASSWORD("//*[@id=\"password\"]"),
 	VIEWAD_BCHECKANSWERS("/html/body/div[2]/div[4]/a/button"),
-	VIEWAD_LOWNER("/html/body/div[2]/div[3]/div[1]/div/div[2]/span");
-	
+	VIEWAD_LOWNER("/html/body/div[2]/div[3]/div[1]/div/div[2]/span"),
+	NAVBAR_BMAKEANAD("/html/body/div[1]/nav/nav/div/ul/li[1]/a"),
+	MAKEANAD_STYPE("//*[@id=\"type\"]"),
+	MAKEANAD_OFFERTYPE("/html/body/div/div[3]/form/div[1]/select/option[2]"),
+	MAKEANAD_REQUESTTYPE("/html/body/div/div[3]/form/div[1]/select/option[1]"),
+	MAKEANAD_FTITLE("//*[@id=\"title\"]"),
+	MAKEANAD_SCATEGORY("//*[@id=\"category\"]"),
+	MAKEANAD_GARDERINGCATEGORY("/html/body/div/div[3]/form/div[3]/select/option[3]"),
+	MAKEANAD_FBODY("//*[@id=\"body\"]"),
+	MAKEANAD_BPUBLISH("//*[@id=\"publish\"]")
 	;
 	private final String xpath;
 	private XPaths(String xpath) {
