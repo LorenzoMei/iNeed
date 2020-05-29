db023c41048a2d5ba05d8ae6983d6bb6cd5f189d
LorenzoMei <45484506+LorenzoMei@users.noreply.github.com>
2020-05-29 22:41:35 +0200

    #288

--- src/test/web/TestMakeAnAd.java
+++ src/test/web/TestMakeAnAd.java
@@ -1,80 +1,96 @@
 package test.web;
 
+import java.lang.reflect.InvocationTargetException;
+
 import org.junit.Assert;
 
 import org.junit.Test;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import logic.beans.OrderedAdsBean;
+import logic.dao.DAOAd;
+import logic.dao.DAOFactory;
+import logic.dao.DAOSupportedEntities;
+import logic.viewanad.ViewAnAdController;
 
 public class TestMakeAnAd {
 	
 	private static final String URL = "http://localhost:8081/iNeed/";
+	
+	private static final String DRIVER = "webdriver.gecko.driver";
+	private static final Class<? extends WebDriver> DRIVER_CLASS = FirefoxDriver.class;
+	private static final String DRIVER_PATH = "Drivers/geckodriver";
+
 
 	@Test
 	public void MakeAnAd() {
-		System.setProperty("webdriver.gecko.driver", "Drivers/geckodriver");
-		WebDriver driver = new FirefoxDriver();
-		driver.get(URL);
 		
-		String username = "prova";
-		String password = "123";
-		String title = "TITOLO TEST SELENIUM";
-		String category = "Gardering";
-		String type = "OfferAd";
-		String body = "BODY TEST SELENIUM";
-		
-		//Log In to iNeed
-		driver.findElement(By.xpath(XPaths.LOGIN_FUSERNAME.toString())).sendKeys(username);
-		driver.findElement(By.xpath(XPaths.LOGIN_FPASSWORD.toString())).sendKeys(password);
-		driver.findElement(By.xpath(XPaths.LOGIN_BLOGIN.toString())).click();
-		
-		driver.findElement(By.xpath(XPaths.NAVBAR_BMAKEANAD.toString())).click();
-		
-		//Creation of an ad
-		if(type.equals("OfferAd")){
-			driver.findElement(By.xpath(XPaths.MAKEANAD_STYPE.toString())).click();
-			driver.findElement(By.xpath(XPaths.MAKEANAD_OFFERTYPE.toString())).click();
-		}
-		else if(type.equals("RequestAd")) {
-			driver.findElement(By.xpath(XPaths.MAKEANAD_STYPE.toString())).click();
-			driver.findElement(By.xpath(XPaths.MAKEANAD_REQUESTTYPE.toString())).click();
-		}
-		
-		driver.findElement(By.xpath(XPaths.MAKEANAD_FTITLE.toString())).sendKeys(title);
-		
-		driver.findElement(By.xpath(XPaths.MAKEANAD_SCATEGORY.toString())).click();
-		driver.findElement(By.xpath(XPaths.MAKEANAD_GARDERINGCATEGORY.toString())).click();
-		driver.findElement(By.xpath(XPaths.MAKEANAD_FBODY.toString())).sendKeys(body);
-		
-		driver.findElement(By.xpath(XPaths.MAKEANAD_BPUBLISH.toString())).click();
-		
-		int i = 1;
-		boolean find = true;
-		
-		//search my ad in ViewFlow
-		while(find) {
-			try{
-				String value = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr["+ i +"]/td[2]/a")).getText();
-				//When i found an ad with the same title and type, check if the rest of the fields are the same 
-				if(value.equals(title) && type.equals(driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr["+ i +"]/td[1]")).getText())){
-					Assert.assertEquals(category, driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr["+ i +"]/td[4]")).getText());
-					driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr["+ i +"]/td[2]/a")).click();
-					Assert.assertEquals(body, driver.findElement(By.xpath("//*[@id=\"bodyText\"]")).getText());
-					Assert.assertEquals(username, driver.findElement(By.xpath("/html/body/div[2]/div[3]/div[1]/div/div[2]/span")).getText());
-					break;
-				}
-				i++;
-				
-			}catch(NoSuchElementException e) {
-				find = false;
+		try {
+			System.setProperty(DRIVER, DRIVER_PATH);
+			WebDriver driver = DRIVER_CLASS.getConstructor((Class<?>[])null).newInstance((Object[])null);
+			
+			driver.get(URL);
+			
+			String username = "prova";
+			String password = "123";
+			String title = "TITOLO TEST SELENIUM";
+			String type = "OfferAd";
+			String body = "BODY TEST SELENIUM";
+			int newId = -1;
+			
+			//get last id from database and add 1 because new add has this id
+			DAOAd dao = (DAOAd) DAOFactory.getReference().getDAOReference(DAOSupportedEntities.AD);
+			if(type.compareTo("OfferAd") == 0){
+				newId = dao.getLastOfferId() + 1;
 			}
-				
+			else if(type.compareTo("RequestAd") == 0){
+				newId = dao.getLastRequestId() + 1;
+			}
+			
+			//Log In to iNeed
+			driver.findElement(By.xpath(XPaths.LOGIN_FUSERNAME.toString())).sendKeys(username);
+			driver.findElement(By.xpath(XPaths.LOGIN_FPASSWORD.toString())).sendKeys(password);
+			driver.findElement(By.xpath(XPaths.LOGIN_BLOGIN.toString())).click();
+			
+			driver.findElement(By.xpath(XPaths.NAVBAR_BMAKEANAD.toString())).click();
+			
+			//Creation of an ad
+			if(type.equals("OfferAd")){
+				driver.findElement(By.xpath(XPaths.MAKEANAD_STYPE.toString())).click();
+				driver.findElement(By.xpath(XPaths.MAKEANAD_OFFERTYPE.toString())).click();
+			}
+			else if(type.equals("RequestAd")) {
+				driver.findElement(By.xpath(XPaths.MAKEANAD_STYPE.toString())).click();
+				driver.findElement(By.xpath(XPaths.MAKEANAD_REQUESTTYPE.toString())).click();
+			}
+			
+			driver.findElement(By.xpath(XPaths.MAKEANAD_FTITLE.toString())).sendKeys(title);
+			
+			driver.findElement(By.xpath(XPaths.MAKEANAD_SCATEGORY.toString())).click();
+			driver.findElement(By.xpath(XPaths.MAKEANAD_GARDERINGCATEGORY.toString())).click();
+			driver.findElement(By.xpath(XPaths.MAKEANAD_FBODY.toString())).sendKeys(body);
+			
+			driver.findElement(By.xpath(XPaths.MAKEANAD_BPUBLISH.toString())).click();
+			
+			driver.close();
+			
+			//Compare id
+			ViewAnAdController controller = ViewAnAdController.getReference();
+			OrderedAdsBean bean = new OrderedAdsBean();
+			controller.listAllAds(bean);
+			
+			for(int i = 0; i < bean.getNumOfAds(); i++) {
+				Assert.assertEquals(newId, bean.getId(i));
+			}
+		
+		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
+				| NoSuchMethodException | SecurityException e) {
+			e.printStackTrace();
 		}
 		
-		driver.close();		
 	}
 
 }
--- src/test/web/TestValidateAFavor.java
+++ src/test/web/TestValidateAFavor.java
@@ -1,5 +1,7 @@
 package test.web;
 
+import java.lang.reflect.InvocationTargetException;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -11,27 +13,40 @@ public class TestValidateAFavor {
 	private static final String URL = "http://localhost:8081/iNeed/";
 	private static final String SUCCESS = "Favor validated successfully";
 	
+	private static final String DRIVER = "webdriver.gecko.driver";
+	private static final Class<? extends WebDriver> DRIVER_CLASS = FirefoxDriver.class;
+	private static final String DRIVER_PATH = "Drivers/geckodriver";
+	
 	@Test
 	public void ValidateAFavor() {
-		System.setProperty("webdriver.gecko.driver", "Drivers/geckodriver");
-		WebDriver driver = new FirefoxDriver();
-		driver.get(URL);
 		
-		String username = "prova";
-		String password = "123";
+		try {
+			System.setProperty(DRIVER, DRIVER_PATH);
+			WebDriver driver = DRIVER_CLASS.getConstructor((Class<?>[])null).newInstance((Object[])null);
+			
+			driver.get(URL);
+			
+			String username = "prova";
+			String password = "123";
+			
+			//Log In to iNeed
+			driver.findElement(By.xpath(XPaths.LOGIN_FUSERNAME.toString())).sendKeys(username);
+			driver.findElement(By.xpath(XPaths.LOGIN_FPASSWORD.toString())).sendKeys(password);
+			driver.findElement(By.xpath(XPaths.LOGIN_BLOGIN.toString())).click();
+			
+			//Validate a favor
+			driver.findElement(By.xpath(XPaths.VALIDATEAFAVOR_BVALIDATEAFAVOR.toString())).click();
+			driver.findElement(By.xpath(XPaths.VALIDATEAFAVOR_BVALIDATE.toString())).click();
+			driver.findElement(By.xpath(XPaths.VALIDATEAFAVOR_BGO.toString())).click();
+			
+			String result = driver.findElement(By.xpath(XPaths.VALIDATEAFAVOR_TRESULT.toString())).getText();
+			Assert.assertEquals(SUCCESS, result);
+			
+		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
+				| NoSuchMethodException | SecurityException e) {
+			e.printStackTrace();
+		}
 		
-		//Log In to iNeed
-		driver.findElement(By.xpath(XPaths.LOGIN_FUSERNAME.toString())).sendKeys(username);
-		driver.findElement(By.xpath(XPaths.LOGIN_FPASSWORD.toString())).sendKeys(password);
-		driver.findElement(By.xpath(XPaths.LOGIN_BLOGIN.toString())).click();
-		
-		//Validate a favor
-		driver.findElement(By.xpath(XPaths.VALIDATEAFAVOR_BVALIDATEAFAVOR.toString())).click();
-		driver.findElement(By.xpath(XPaths.VALIDATEAFAVOR_BVALIDATE.toString())).click();
-		driver.findElement(By.xpath(XPaths.VALIDATEAFAVOR_BGO.toString())).click();
-		
-		String result = driver.findElement(By.xpath(XPaths.VALIDATEAFAVOR_TRESULT.toString())).getText();
-		Assert.assertEquals(SUCCESS, result);
 	}
 
 }
